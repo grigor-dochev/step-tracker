fde3c41ae6c0a51a8ec2645439f0a0c13d92852f
grigor-dochev <48100639+grigor-dochev@users.noreply.github.com>
2024-08-03 19:54:02 +0200

    created xcode project

---
+++ Step Tracker/Step_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Step_TrackerApp.swift
+//  Step Tracker
+//
+//  Created by Grigor Dochev on 03.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct Step_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
